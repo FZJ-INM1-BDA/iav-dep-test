a07392045ec6ac270ab59a6c12d21127fe30085d
Xiao Gui <xgui3783@gmail.com>
2022-07-07 10:39:04 +0200

    update kg-query monitoring

--- kg-query/v1/index.js
+++ kg-query/v1/index.js
@@ -1,51 +1,10 @@
-const request = require('request')
-const { expect } = require('chai')
-require('mocha')
-
-const apiEndPoint = `https://kg.humanbrainproject.eu/query/`
-const JWT_ACCESS_TOKEN = process.env.JWT_ACCESS_TOKEN
-
 const {
-  OAUTH_V2_SA_CLIENT_ID,
-  OAUTH_V2_SA_CLIENT_SECRET,
-  OAUTH_V2_SA_ENDPOINT='https://iam.ebrains.eu/auth/realms/hbp/protocol/openid-connect',
-  OAUTH_V2_SA_SCOPES='',
   KG_INSTANCE_ID,
-  KG_DS_QUERY_ID="interactiveViewerKgQuery-v1_0",
+  JWT_ACCESS_TOKEN,
+  KG_DS_QUERY_ID = "interactiveViewerKgQuery-v1_0",
 } = process.env
+const apiEndPoint = `https://kg.humanbrainproject.eu/query/`
 
-function getAccessToken(){
-  console.log(`getting access token from ${OAUTH_V2_SA_ENDPOINT}`)
-  return new Promise((rs, rj) => {
-    request({
-      uri: `${OAUTH_V2_SA_ENDPOINT}/token`,
-      method: 'POST',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      form: {
-        grant_type: 'client_credentials',
-        client_id: OAUTH_V2_SA_CLIENT_ID,
-        client_secret: OAUTH_V2_SA_CLIENT_SECRET,
-        scope: OAUTH_V2_SA_SCOPES
-      }
-    }, (error, resp, body) => {
-      if (error) return rj(error)
-      if (resp.statusCode >= 400) {
-        const {
-          statusCode,
-          statusMessage,
-          body
-        } = resp
-        return rj({ statusCode, statusMessage, body })
-      }
-      const json = JSON.parse(body)
-      rs(json['access_token'])
-    })
-  })
-}
-
-let fetchedAccessToken
 
 const queryBody = {
   "@context": {
@@ -76,12 +35,23 @@ const queryBody = {
   ]
 }
 
+const { getAccessToken } = require("../token")
+require("mocha")
+const request = require('request')
+const { expect } = require('chai')
+
 describe(`> REST end for kg @ ${apiEndPoint}`, () => {
 
+  let fetchedAccessToken
   before(done => {
+    if (!!JWT_ACCESS_TOKEN) {
+      console.warn(`JWT_ACCESS_TOKEN set, using it rather than fetching client access token`)
+      return done()
+    }
     getAccessToken()
       .then(token => {
         fetchedAccessToken = token
+        console.log(token)
         done()
       })
       .catch(done)
