61f212c0a59ddc5dcd7f7eef4fde909b1ca48799
Xiao Gui <xgui3783@gmail.com>
2024-08-18 12:30:32 +0200

    check pt assignment

---
+++ siibra/python/check_point_assignment.py
@@ -0,0 +1,10 @@
+import siibra
+
+def main():
+    m = siibra.get_map('3.0.3', 'icbm 152', 'statistical')
+    pt = siibra.Point([-57, -58, 32], space="icbm 152", sigma_mm=0)
+    asgmt = m.assign(pt)
+    print(asgmt[["region", "map value"]])
+
+if __name__ == "__main__":
+    main()
