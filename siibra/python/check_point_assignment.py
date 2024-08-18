c61f9288705aa87a4bd364e7e966ce8e1a98387b
Xiao Gui <xgui3783@gmail.com>
2024-08-18 12:51:45 +0200

    update for julich brain 3.1

--- siibra/python/check_point_assignment.py
+++ siibra/python/check_point_assignment.py
@@ -1,7 +1,7 @@
 import siibra
 
 def main():
-    m = siibra.get_map('3.0.3', 'icbm 152', 'statistical')
+    m = siibra.get_map('3.1', 'icbm 152', 'statistical')
     pt = siibra.Point([-57, -58, 32], space="icbm 152", sigma_mm=0)
     asgmt = m.assign(pt)
     print(asgmt[["region", "map value"]])
