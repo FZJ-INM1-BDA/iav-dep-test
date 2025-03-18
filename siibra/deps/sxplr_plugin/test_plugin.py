ef72a8ac2871bb4ccb1a99fede34896b8ff850e4
Xiao Gui <xgui3783@gmail.com>
2025-03-18 16:39:37 +0100

    fix tests

--- siibra/deps/sxplr_plugin/test_plugin.py
+++ siibra/deps/sxplr_plugin/test_plugin.py
@@ -1,17 +1,17 @@
 import os
 import requests
 
-ENDPOINT = os.getenv("SIIBRA_DEPS_JUGEX_ENDPOINT", "https://siibra-jugex.apps.tc.humanbrainproject.eu")
+ENDPOINT = os.getenv("SIIBRA_DEPS_PLUGIN_ENDPOINT", "https://siibra-jugex.apps.tc.humanbrainproject.eu/viewer_plugin")
 
 def test_plugin_html():
-    resp = requests.get(f"{ENDPOINT}/viewer_plugin/manifest.json")
+    resp = requests.get(f"{ENDPOINT}/manifest.json")
     resp.raise_for_status()
     resp_json = resp.json()
     assert resp_json.get("siibra-explorer"), f"key siibra-explorer is expected, but not found in {resp_json}"
     assert resp_json.get("iframeUrl"), f"key iframeUrl is expected, but not found in {resp_json}"
     iframe_url = resp_json.get("iframeUrl")
 
-    resp = requests.get(f"{ENDPOINT}/viewer_plugin/{iframe_url}")
+    resp = requests.get(f"{ENDPOINT}/{iframe_url}")
     resp.raise_for_status()
     resp_text = resp.text
     assert "html" in resp_text
